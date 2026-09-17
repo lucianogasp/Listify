d74d1d59333e7d86b6cb46a5d83d23eb2f309fbc
Luciano Gasparin <lucianogasparin57@gmail.com>
2026-09-17 03:48:25 -0300

    test: add testes para itemsService.deleteItem

--- Backend/tests/services/items.spec.js
+++ Backend/tests/services/items.spec.js
@@ -1,15 +1,39 @@
 import { describe, expect, jest } from "@jest/globals";
 
+const mockAsyncGetItemById = {
+  asyncGetItemById: jest.fn()
+}
+const mockAsyncGetListById = {
+  asyncGetListById: jest.fn()
+}
+
 jest.unstable_mockModule(
   '#repositories/items.repositories.js',
   () => {
     return {
       default: {
         getItemsByUserId: jest.fn(),
-        createItem: jest.fn()
+        createItem: jest.fn(),
+        deleteItem: jest.fn()
       }
     }
   }
+);
+jest.unstable_mockModule(
+  '#services/ItemQuery.js',
+  () => ({
+    ItemQuery: jest.fn().mockImplementation(() => {
+      return mockAsyncGetItemById;
+    })
+  })
+);
+jest.unstable_mockModule(
+  '#services/ListQuery.js',
+  () => ({
+    ListQuery: jest.fn().mockImplementation(() => {
+      return mockAsyncGetListById;
+    })
+  })
 );
 
 describe('Testing itemsService.getItemsByUserId', () => {
@@ -95,9 +119,105 @@ describe('Testing itemsService.createItem', () => {
   });
 });
 
-// describe('Testing itemsService.deleteItem', () => {
+describe('Testing itemsService.deleteItem', () => {
 
-//   it('should return an object containing the item deleted form repository');
-//   it('should throw an Error with an object alert message if getListById return undefined');
-//   it('should throw an Error with an object alert message if getItemById return undefined');
-// });
+  it('should return an object containing the item deleted form repository', async () => {
+
+    // Arrangemet
+    const item_id = 1;
+    const list_id = 1;
+    const userId = 1;
+    const mockTimes = 1;
+
+    mockAsyncGetListById.asyncGetListById.mockResolvedValue({
+      id: 1, 
+      user_id: 1, 
+      name: "compras", 
+      description: "lista de compras" 
+    });
+    mockAsyncGetItemById.asyncGetItemById.mockResolvedValue({
+      id: 1,
+      list_id: 1,
+      title: "pão",
+      is_prioritized: "prioridade",
+      status: "status"
+    });
+    const {default: itemsRepository} = await import('#repositories/items.repositories.js');
+    itemsRepository.deleteItem.mockResolvedValue({message: 'item deleted successfully!'});
+    const {default: itemsServices} = await import('#services/items.service.js');
+
+    // Act
+    const result = await itemsServices.deleteItem(item_id, list_id, userId);
+
+    // Assertion
+    expect(mockAsyncGetListById.asyncGetListById).toHaveBeenCalled();
+    expect(mockAsyncGetListById.asyncGetListById).toHaveBeenCalledWith(list_id);
+    expect(mockAsyncGetListById.asyncGetListById).toHaveBeenCalledTimes(mockTimes);
+
+    expect(mockAsyncGetItemById.asyncGetItemById).toHaveBeenCalled();
+    expect(mockAsyncGetItemById.asyncGetItemById).toHaveBeenCalledWith(item_id);
+    expect(mockAsyncGetItemById.asyncGetItemById).toHaveBeenCalledTimes(mockTimes);
+    
+    expect(itemsRepository.deleteItem).toHaveBeenCalled();
+    expect(itemsRepository.deleteItem).toHaveBeenCalledWith(item_id, list_id, userId);
+    expect(itemsRepository.deleteItem).toHaveBeenCalledTimes(mockTimes);
+
+    expect(result).toEqual({
+      message: "item deleted successfully!",
+      id: 1,
+      list_id: 1,
+      title: "pão",
+      is_prioritized: "prioridade",
+      status: "status"
+    });
+  });
+
+  it('should throw an Error with an object alert message if getListById return undefined', async () => {
+
+    // Arrangement
+    const list_id = 999;
+    const mockTimes = 1;
+
+    mockAsyncGetListById.asyncGetListById.mockResolvedValue(undefined);
+    const {default: itemsService} = await import('#services/items.service.js');
+    
+    // Act
+    const result = itemsService.deleteItem;
+
+    // Assertion
+    await expect(result(null, list_id, null)).rejects.toThrow('list associated with the item does not exists to be deleted!');
+
+    expect(mockAsyncGetListById.asyncGetListById).toHaveBeenCalled();
+    expect(mockAsyncGetListById.asyncGetListById).toHaveBeenCalledWith(list_id);
+    expect(mockAsyncGetListById.asyncGetListById).toHaveBeenCalledTimes(mockTimes);
+  });
+  it('should throw an Error with an object alert message if getItemById return undefined', async () => {
+
+    // Arrangement
+    const item_id = 999;
+    const list_id = 1;
+    const mockTimes = 1;
+
+    mockAsyncGetListById.asyncGetListById.mockResolvedValue({
+      id: 1, 
+      user_id: 1, 
+      name: "compras", 
+      description: "lista de compras" 
+    });
+    mockAsyncGetItemById.asyncGetItemById.mockResolvedValue(undefined);
+    const {default: itemsService} = await import("#services/items.service.js");
+
+    // Act
+    const result = itemsService.deleteItem;
+
+    // Assertion
+    await expect(result(item_id, list_id, null)).rejects.toThrow('item does not exists to be deleted!');
+
+    expect(mockAsyncGetListById.asyncGetListById).toHaveBeenCalled();
+    expect(mockAsyncGetListById.asyncGetListById).toHaveBeenCalledWith(list_id);
+    expect(mockAsyncGetListById.asyncGetListById).toHaveBeenCalledTimes(mockTimes);
+    expect(mockAsyncGetItemById.asyncGetItemById).toHaveBeenCalled();
+    expect(mockAsyncGetItemById.asyncGetItemById).toHaveBeenCalledWith(item_id);
+    expect(mockAsyncGetItemById.asyncGetItemById).toHaveBeenCalledTimes(mockTimes);
+  });
+});
